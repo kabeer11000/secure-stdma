e88e772b3e03e8e9271203b0e884ee30c90a6c14
kabeer11000 <kabeer11000@gmail.com>
2026-03-31 01:52:05 +0500

    refactor: Updated configurations, and run tests

---
+++ sources/build/ns3/stdma-module.h
@@ -0,0 +1,16 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_STDMA
+    
+
+// Module headers:
+#include "stdma-header.h"
+#include "stdma-helper.h"
+#include "stdma-mac-helper.h"
+#include "stdma-mac.h"
+#include "stdma-net-device.h"
+#include "stdma-slot-manager.h"
+#endif
